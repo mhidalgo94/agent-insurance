3bffc65228ff6094212270960d075e100737d86f
Mario Sergio <marito@Marito-Macbook-Pro.local>
2024-11-04 09:04:52 -0500

    Starting proyect leo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'bulma/css/bulma.min.css';
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="container">
+      <h1 className="title">Hello, Bulma in React!</h1>
+      <button className="button is-primary">Primary Button</button>
+    </div>
+    </>
+  )
+}
+
+export default App
