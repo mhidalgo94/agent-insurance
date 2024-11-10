106e4d02541ee1eaf2beeb15c708a3a23eaf17e3
Mario Sergio <marito@Marito-Macbook-Pro.local>
2024-11-10 16:10:12 -0500

    Fix className components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -38,10 +38,10 @@ const Navbar = () => {
           <li><a href="#contact">Contact</a></li>
         </ul>
         <div>
-        <a href="#contact" class="contact-link">
-            Contact Us <span class="arrow">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right bold-icon" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" stroke="currentColor" stroke-width="1.2"/>
+        <a href="#contact" className="contact-link">
+            Contact Us <span className="arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" stroke="currentColor" strokeWidth="1.2"/>
             </svg>
           </span>
         </a>
