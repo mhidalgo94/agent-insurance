6e50d2f783dff9c15515a981c00ebfc3b8a9a85b
mhidalgo94 <mhidalgodev@outlook.com>
2024-11-12 21:02:59 -0500

    Adding section2 page card section

---
+++ src/components/body/section2/section2.jsx
@@ -0,0 +1,56 @@
+
+const Section2 = ()=>{
+    return (
+        <section class="section cards-section">
+            <div class="container">
+                <div class="columns is-multiline is-centered">
+            
+                    <div class="column is-12-mobile is-6-tablet is-4-desktop">
+                        <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                            <img src="https://via.placeholder.com/300" alt="Card Image 1"/>
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <p class="title is-4">Tailored Insurance</p>
+                            <p>Find the perfect plan that fits your needs and budget.</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="column is-12-mobile is-6-tablet is-4-desktop">
+                        <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                            <img src="https://via.placeholder.com/300" alt="Card Image 2" />
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <p class="title is-4">Expert Guidance</p>
+                            <p>Receive support from experienced professionals.</p>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div class="column is-12-mobile is-6-tablet is-4-desktop">
+                        <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                            <img src="https://via.placeholder.com/300" alt="Card Image 3"/>
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <p class="title is-4">Comprehensive Coverage</p>
+                            <p>Get access to a variety of coverage options for peace of mind.</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+
+export default Section2;
