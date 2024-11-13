949e42fd50ba0c554ce10293feba037766895e62
mhidalgo94 <mhidalgodev@outlook.com>
2024-11-12 23:46:09 -0500

    Adding title reviews sections

--- src/components/body/reviews/reviews.jsx
+++ src/components/body/reviews/reviews.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Reviews = ()=> {
   return (
-    <section className="sections">
-        <p>probando</p>
+    <section class="hero mt-5">
+        <div class="hero-body">
+            <div style={{margin:"0 auto", textAlign:'center'}}>
 
-
+                <p className='title is-4 has-text-primary'>Trusted by Many for Reliable Insurance Solutions</p>
+                <p className='title is-3 has-text-grey-dark'>What Our Clients Say</p>
+            </div>
+        </div>
     </section>
   )
 }
